e53fcb6b40620c8f892953f4596b672d9466937e
Adderally <56490828+Adderally@users.noreply.github.com>
2021-02-21 15:53:58 -0600

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -88,6 +88,7 @@ namespace Vet
             foreach (Process process in processes)
             {
                 data[0] = process.Id.ToString();
+                
 
                 try
                 {
@@ -102,7 +103,7 @@ namespace Vet
                 }
 
                 data[1] = process.ProcessName + ".exe";
-
+                
 
                 item = new ListViewItem(data);
 
@@ -176,26 +177,23 @@ namespace Vet
         void processStartEvent_EventArrived(object sender, EventArrivedEventArgs e)
         {
 
-            
-
-            string p_name;
-            p_name = e.NewEvent.Properties["ProcessName"].Value.ToString();
-
-
-
-            
-
 
             ListViewItem item;
-            string[] data = new string[3];            
+
+            string p_name;                       
             string pid;
             string owner;
             string file_name;
-            bool   found = false;
 
-            
+            string[] data = new string[3];
+            bool found    = false;
+
+
+
             pid    = Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value).ToString();
-            
+            p_name = e.NewEvent.Properties["ProcessName"].Value.ToString();
+
+
             if (string.IsNullOrEmpty(pid))
             
 
@@ -215,20 +213,54 @@ namespace Vet
 
             if (found)
             {
+                
 
-
-                foreach (var blocked_item in blocked_process_list)
+                foreach (var blocked_item in blockeprocesses_lb.Items)
                 {
-                    if (p_name == blocked_item)
+                    if (p_name == blocked_item.ToString())
                     {
+
+
+                        DateTime starttime = DateTime.Now;
+
+
 
                         using (Process process = Process.GetProcessById(Int32.Parse(pid)))
                         {
-                            process.Kill();
+                            if (terminate_blocked_process_rb.Checked)
+                            {
+
+
+                                watchdog_logger.Items.Add("Process " + p_name + " (" + pid + ")" + " started. // " + starttime);
+                                process.Kill();
+                                DateTime killtime = DateTime.Now;
+                                watchdog_logger.Items.Add("Process " + p_name + " (" + pid + ")" + " terminated! // " + killtime);
+
+
+                            }
+
+                            if (suspend_blocked_process_rb.Checked)
+                            {
+
+
+                                watchdog_logger.Items.Add("Process " + p_name + " (" + pid + ")" + " started. // " + starttime);
+                                process_essentials.freeze_process(process.Id);
+                                DateTime killtime = DateTime.Now;
+                                watchdog_logger.Items.Add("Process " + p_name + " (" + pid + ")" + " suspended! // " + killtime);
+
+
+                            }
+                            
                         }
 
+                        return;     //  If process is part of user "WatchDog" it is either getting suspended or terminated
+                                    //  on start, so there is really no need to waste process time and memory filling out the listview of a "dead" / 
+                                    //  "soon to be dead" process ...
                     }
                 }
+
+
+                /* IF PROCESS IS NOT IN WATCHDOG ...*/
 
 
                 data[1] = p_name;
@@ -245,7 +277,9 @@ namespace Vet
                     catch (Exception)
                     {
                         data[2] = "Access Denied";      //  System processes that require trust certs.
-                    }                    
+                    }
+
+                    
                 }
             }
           
@@ -304,11 +338,14 @@ namespace Vet
         {
             if (e.Button == MouseButtons.Right)
             {
+
                 var focusedItem = processes_listview.FocusedItem;
+
                 if (focusedItem != null && focusedItem.Bounds.Contains(e.Location))
                 {
                     rightclick_options_menu.Show(Cursor.Position);
                 }
+
             }
         }
 
@@ -329,14 +366,11 @@ namespace Vet
                     path = "/select, \"" + pid.MainModule.FileName.Substring(0, pid.MainModule.FileName.LastIndexOf(program_name)) + "\"";
                     Process.Start("explorer.exe", "-p" + path);
                 }
-                catch (Win32Exception)
+                catch (UnauthorizedAccessException)
                 {
                     //
                 }
-                catch (Exception)
-                {
-                    //
-                }
+                
             }
   
         }
@@ -347,9 +381,17 @@ namespace Vet
             void rcm_kill_process_Click(object sender, EventArgs e)
         {
             ListViewItem item = processes_listview.SelectedItems[0];
+
             using (Process pid = Process.GetProcessById(Int32.Parse(item.Text)))
             {
-                pid.Kill();
+                try
+                {
+                    pid.Kill();
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    //
+                }
             }
         }
 
@@ -433,23 +475,7 @@ namespace Vet
         private
             void processes_listview_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Down)
-            {
-                processes_listview.BackColor        = Color.Black;
-                processes_listview.ForeColor        = Color.LightGreen;
-                toolstrip.BackColor                 = Color.Black;
-                toolstrip_processes_count.ForeColor = Color.LightGreen;
-                
-            }
-
-            if (e.KeyCode == Keys.Up)
-            {
-                processes_listview.BackColor        = Color.White;
-                processes_listview.ForeColor        = Color.Black;
-                toolstrip.BackColor                 = Color.White;
-                toolstrip_processes_count.ForeColor = Color.Black;
-
-            }
+            
         }
 
 
@@ -457,7 +483,28 @@ namespace Vet
         private
             void rcm_add_process_blocklist_Click(object sender, EventArgs e)
         {
+            ListViewItem item = processes_listview.SelectedItems[0];
+
+
+
+            int process_id      = Int32.Parse(item.Text);
             string program_name = processes_listview.SelectedItems[0].SubItems[1].Text;
+
+            
+
+            using (Process pid = Process.GetProcessById(process_id))
+            {
+                try
+                {
+                    pid.Kill();
+                }               
+                catch (UnauthorizedAccessException)
+                {
+                    //
+                }
+            }
+
+            blockeprocesses_lb.Items.Add(program_name);
             blocked_process_list.Add(program_name);
         }
 
@@ -472,7 +519,7 @@ namespace Vet
 
                 foreach(ListViewItem item in processes_listview.Items)
                 {
-                    if(item.SubItems[1].Text.Contains(searched_for))
+                    if(item.SubItems[1].Text.StartsWith(searched_for))
                     {
 
                         item.Selected = true;
@@ -480,6 +527,53 @@ namespace Vet
                     }
                 }
             }
+        }
+
+        private void tabview_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                processes_listview.BackColor        = Color.Black;
+                processes_listview.ForeColor        = Color.LightGreen;
+                toolstrip.BackColor                 = Color.Black;
+                toolstrip_processes_count.ForeColor = Color.LightGreen;
+                watchdog_tab.BackColor              = Color.Black;
+                processes_tab.BackColor             = Color.Black;
+
+
+                blockeprocesses_lb.BackColor                              = Color.Black;
+                blockeprocesses_lb.ForeColor                              = Color.LightGreen;
+                watchdog_logger.BackColor                                 = Color.Black;
+                watchdog_logger.ForeColor                                 = Color.LightBlue;
+                suspend_blocked_process_rb.ForeColor                      = Color.Gold;
+                terminate_blocked_process_rb.ForeColor                    = Color.Gold;
+                save_blocked_processes_for_next_session_load_cb.ForeColor = Color.Gold;
+
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                processes_listview.BackColor        = Color.White;
+                processes_listview.ForeColor        = Color.Black;
+                toolstrip.BackColor                 = Color.White;
+                toolstrip_processes_count.ForeColor = Color.Black;
+                watchdog_tab.BackColor              = Color.White;
+                processes_tab.BackColor             = Color.White;
+
+
+                blockeprocesses_lb.BackColor                              = Color.White;
+                blockeprocesses_lb.ForeColor                              = Color.Black;
+                watchdog_logger.BackColor                                 = Color.White;
+                watchdog_logger.ForeColor                                 = Color.Black;
+                suspend_blocked_process_rb.ForeColor                      = Color.Black;
+                terminate_blocked_process_rb.ForeColor                    = Color.Black;
+                save_blocked_processes_for_next_session_load_cb.ForeColor = Color.Black;
+            }
+        }
+
+        private void watchdog_setting_gb_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 
